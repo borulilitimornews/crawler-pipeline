5c96eb65a4d99ae2adad3eddfd42193f324aa951
Gabriel de Jesus <gabriel.dejesus@timornews.tl>
2023-05-10 08:04:49 +0100

    Final codes.

--- pipeline/src/collection_stat.py
+++ pipeline/src/collection_stat.py
@@ -90,32 +90,36 @@ class CollectionStatistic:
                 else:
                     extension_counts[extension] = 1
 
-                # Outlinks and Inlinks for each URL
-                response = requests.get(url)
-                if response.status_code == 200:
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    links = soup.find_all('a')
-                    
-                    outlink_count = 0
-                    inlink_count = 0
-                    for link in links:
-                        href = link.get('href')
-                        if href and (href.startswith('http://') or href.startswith('https://')):
-                            if domain not in href:
-                                outlink_count += 1
-                            else:
+                try:
+                    # Outlinks and Inlinks for each URL
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content, 'html.parser')
+                        links = soup.find_all('a')
+                        
+                        outlink_count = 0
+                        inlink_count = 0
+                        for link in links:
+                            href = link.get('href')
+                            if href and (href.startswith('http://') or href.startswith('https://')):
+                                if domain not in href:
+                                    outlink_count += 1
+                                else:
+                                    inlink_count += 1
+                            elif href and not href.startswith('#'):
                                 inlink_count += 1
-                        elif href and not href.startswith('#'):
-                            inlink_count += 1
 
-                    outlink_count_list.append(outlink_count)
-                    inlink_count_list.append(inlink_count)
-                    self.url_in_out_links.save_corpus(f"Url: {url}, Outlink: {outlink_count}, Inlink: {inlink_count}")
-                else:
+                        outlink_count_list.append(outlink_count)
+                        inlink_count_list.append(inlink_count)
+                        self.url_in_out_links.save_corpus(f"Url: {url}, Outlink: {outlink_count}, Inlink: {inlink_count}")
+                    else:
+                        continue
+                except (requests.exceptions.RequestException, requests.exceptions.ConnectionError, requests.exceptions.HTTPError, 
+                        requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.URLRequired):
                     continue
 
         # Save the inlinks and outlinks summary        
-        stat_inlinks_outlinks = f""" The statistics of the collection:
+        stat_inlinks_outlinks = f""" Statistics of the collection:
         ========================================
         Total web pages (urls) processed: {total_documents}\n
         Max outlinks: {max(outlink_count_list)}, Min outlinks: {min(outlink_count_list)}, Average oulinks: {np.mean(outlink_count_list):.2f}
