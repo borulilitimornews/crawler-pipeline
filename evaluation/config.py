304f606c56c8cb143d143762df49b1314139203d
Gabriel de Jesus <timornewstl@gmail.com>
2023-05-30 17:47:13 +0100

    Update final codes.

--- evaluation/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# File paths
-MAIN_CORPUS_FILE_PATH = Path("pipeline/data/final_corpus.txt")
-SAMPLE_DIRECTORY_PATH = Path("evaluation/data/")
-
-# Total of samples and document segments
-TOTAL_DATA_SAMPLES = 6
-TOTAL_DOCS_SEGMENTS = 50
