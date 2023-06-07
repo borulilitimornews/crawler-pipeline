4453bf87abf52d3b1871b81c65562bc007ce07b3
Gabriel de Jesus <timornewstl@gmail.com>
2023-06-07 19:51:11 +0100

    Update crawler.sh

--- bin/crawler.sh
+++ bin/crawler.sh
@@ -5,24 +5,24 @@
 
 # # Generate seed words and seed URLS
 # # Loop to run the seeder.py script  times
-# for i in {1..10}; do
-#    echo "Generating seed words and seed URLS for the $i time ..."
-#    python3 ./pipeline/seeder.py
-# done
+for i in {1..10}; do
+   echo "Generating seed words and seed URLS for the $i time ..."
+   python3 ./pipeline/seeder.py
+done
 
 # # Crawling the World Wide Web
-# echo "Crawling the World Wide Web ..."
-# cd nutch
-# ./bin/crawl -i -s urls/ --hostdbupdate --hostdbgenerate crawl/ 15
-# cd ..
+echo "Crawling the World Wide Web ..."
+cd nutch
+./bin/crawl -i -s urls/ --hostdbupdate --hostdbgenerate crawl/ 15
+cd ..
 # Crawling concluded
-# echo "The crawling have been successfully concluded."
+echo "The crawling have been successfully concluded."
 
-# # Construct text corpus
-# echo "Constructing text corpus ..."
-# python3 ./pipeline/construct_corpus.py
-# # Corpus concluded
-# echo "The corpus have been successfully generated."
+# Construct text corpus
+echo "Constructing text corpus ..."
+python3 ./pipeline/construct_corpus.py
+# Corpus constructed
+echo "The corpus have been successfully generated."
 
 # Generate collection statistic
 echo "Generating statistic for the collection ..."
